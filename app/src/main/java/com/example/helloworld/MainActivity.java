ccaf848282b4e9e4218c0056b7bd28397987b7b9
Patrick Granberg <patgra91@gmail.com>
2020-09-28 17:15:26 +0200

    Increment counter in TextView when button pressed.

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mShowCount = (TextView) findViewById(R.id.show_count);
 
         Log.d(LOG_TAG,"Message world");
         Log.d("MainActivity", "Hello world");
@@ -26,5 +31,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+        //mCount++;
     }
 }
